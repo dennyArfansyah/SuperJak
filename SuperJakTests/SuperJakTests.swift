b1daca35945f3c21557e1d80370bb76262650dbb
Denny Arfansyah <denny.arfansyah@ist.id>
2025-01-14 14:17:55 +0700

    remove unused files

--- SuperJakTests/SuperJakTests.swift
+++ SuperJakTests/SuperJakTests.swift
@@ -10,22 +10,4 @@ import XCTest
 
 final class SuperJakTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
-    func test_Aritmathic() {
-        let sum = 1+1
-        XCTAssertEqual(sum, 2)
-    }
-    
-    func test_stringEqualizer() {
-        let item1 = "Hello World"
-        XCTAssertNotEqual(item1, "Its me Denny")
-    }
-
 }
